07be1057def856d9763550c85f8e8a6c85414f93
xheavenic <73401841+xheavenic@users.noreply.github.com>
2023-02-13 15:19:04 +0800

    update

--- first-training.py
+++ first-training.py
@@ -1,2 +1,12 @@
+
+
+#csv = wget.download("https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBM-DS0321EN-SkillsNetwork/datasets/spacex_launch_dash.csv")
+min_payload = 0
+max_payload = 5000
 import pandas as pd
-print('test')
+spacex_df = pd.read_csv("https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBM-DS0321EN-SkillsNetwork/datasets/spacex_launch_dash.csv")
+spacex_df.dtypes
+filtered_df = spacex_df[(spacex_df['Payload Mass (kg)'] > min_payload) & (spacex_df['Payload Mass (kg)'] < max_payload)]
+  
+#filtered_df = filtered_df.groupby(['Launch Site', 'class']).size().reset_index(name='class_count')
+print(filtered_df)
